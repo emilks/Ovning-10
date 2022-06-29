7cebfa0d31fde88a91c55461901c74a10f4134f5
emilks <emil.keski@gmail.com>
2022-06-29 15:08:36 +0200

    first commit

---
+++ Star Wars/scripts.js
@@ -0,0 +1,48 @@
+const output = document.querySelector('#output');
+const form = document.querySelector('#inputForm');
+
+
+form.addEventListener('submit', getApi);
+
+
+//function(e)
+
+function getApi(e){
+    e.preventDefault();
+    console.log("Pog Pog Pog");
+    let item = document.getElementById("item").value;
+    document.getElementById("item").value = "";
+    /*Skriv din kod här*/
+    fetch("https://www.swapi.tech/api/people/?name="+item)
+    .then(res => res.json())
+    .then(data =>
+        {
+            console.log(data);
+            let char = data.result[0].properties;
+            //let s = `Height: ${char.height}, Mass: ${char.mass}, Gender: ${char.gender}, Hair Color: ${char.hair_color}`;
+            
+            output.innerHTML =
+            `
+            ${char.name}
+            Height: ${char.height}
+            Mass: ${char.mass}
+            Gender: ${char.gender}
+            Hair Color: ${char.hair_color}
+            `
+            /*output.innerHTML =
+            `
+            <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${char.name}</h5>
+              <p class="card-text">Height: ${char.height}</p>
+              <p class="card-text">Mass: ${char.mass}</p>
+              <p class="card-text">Gender: ${char.gender}</p>
+              <p class="card-text">Hair Color: ${char.hair_color}</p>
+            </div>
+            </div>
+            `*/
+        } )
+    .catch(err => console.error(err))
+    
+    //let s = `Height: ${value1}, Mass: ${value2}, Gender: ${test}, Hair Color: ${test}`;
+}
